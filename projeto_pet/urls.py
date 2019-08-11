f6aaf28aecaefffea2f5d4850431fa56fcbaefb4
Deise Koreeda <deise.koreeda@gmail.com>
2019-08-10 23:29:23 -0300

    chore: fix api-auth url path

--- projeto_pet/urls.py
+++ projeto_pet/urls.py
@@ -19,14 +19,18 @@ from users.views import home, new_user, auth_login, logout
 # Adicionando bibliotecas para exibição de midias
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('pets/', include('pets.urls')),
     path('new/', new_user, name='create_user'),
     path('login/', auth_login),
     path('logout/', logout),
     path('users/', home),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 # Adicionando bibliotecas para exibição de midias
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
