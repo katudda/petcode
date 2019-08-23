938ad224e473c9c1c608dc94421240a060db1f74
Deise Koreeda <deise.koreeda@gmail.com>
2019-08-22 22:46:06 -0300

    chore: delete extra models

--- petcode/pets/admin.py
+++ petcode/pets/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Pet, PetType, CategoryStatus, Category, CategoryAdmin, Image
+from .models import Pet, PetType, CategoryStatus, Category, Image
 
 admin.site.register(Pet)
 admin.site.register(Image)
 admin.site.register(PetType)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category)
 admin.site.register(CategoryStatus)
